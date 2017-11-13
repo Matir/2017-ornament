366b59904bce7319d50b6e514a5f8b24d32c502c
David Tomaschik <david@systemoverlord.com>
2017-11-12 19:47:48 -0800

    Faster but less tunable firmware.

--- src/main.c
+++ src/main.c
@@ -9,11 +9,13 @@
 #define MAX_BRIGHTNESS_MASK PWM_FRAME_MASK
 #define TIMER_INTERVAL F_CPU/(DESIRED_FRAMERATE*(PWM_FRAME_MASK+1))
 
-const uint8_t MAX_BRIGHTNESS[NUM_LEDS] = {0x7F, 0x7F, 0x7F, 0x7F, 0x7F, 0x7F};
-const uint8_t RAMP_UP_TIME[NUM_LEDS] =   {0x7F, 0x4F, 0x3F, 0x1F, 0x04, 0x12};
-const uint8_t RAMP_DOWN_TIME[NUM_LEDS] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-// INTERVAL is 4x as long as ramp up/down time
-const uint8_t INTERVAL[NUM_LEDS] =       {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+// Brightness divisors
+const uint8_t MAX_BRIGHTNESS_SCALE[NUM_LEDS] = {0, 0, 0, 0, 0, 0};
+// Scaling factors
+const uint8_t RAMP_UP_SPEED[NUM_LEDS] =   {0x7F, 0x4F, 0x3F, 0x1F, 0x04, 0x12};
+const uint8_t RAMP_DOWN_SPEED[NUM_LEDS] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+// INTERVAL is 2x as long as ramp up/down time
+const uint8_t OFF_SPEED[NUM_LEDS] =       {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 // Current brightness
 uint8_t brightness[NUM_LEDS]     = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
@@ -22,8 +24,10 @@ uint16_t px_frame_no[NUM_LEDS]   = {0};
 
 // Update frame
 volatile uint8_t update_frame = 1;
+#ifdef DEBUG
 volatile uint32_t frame_id = 0;
 volatile uint8_t running_brightness = 0;
+#endif
 
 // Declarations
 void setup(void);
@@ -61,7 +65,9 @@ void main(void) {
     if (update_frame) {
       cli();
       update_loop_step();
+#ifdef DEBUG
       frame_id++;
+#endif
       update_frame = 0;
       sei();
     }
@@ -79,40 +85,49 @@ void update_loop_step() {
 }
 
 void calc_brightness(uint8_t which) {
+#ifdef DEBUG
   running_brightness = 1;
-  uint32_t brightness_tmp;
-  uint16_t max_frame = (
-      RAMP_UP_TIME[which] +
-      RAMP_DOWN_TIME[which] +
-      INTERVAL[which] * 4);
-  uint16_t cur_frame = px_frame_no[which] + 1;
-  if (cur_frame > max_frame)
-    cur_frame = 0;
-  px_frame_no[which] = cur_frame;
+#endif
+  uint8_t brightness_tmp;
+  uint16_t cur_frame = px_frame_no[which];
 
-  if (cur_frame < RAMP_UP_TIME[which]) {
-    brightness_tmp = MAX_BRIGHTNESS[which];
-    brightness_tmp *= cur_frame;
-    brightness_tmp /= RAMP_UP_TIME[which];
+  // Ramp up phase
+  if (cur_frame < 0x3FFF) {
+    // Scales to [0-127]
+    brightness_tmp = (cur_frame >> 7);
+    cur_frame += RAMP_UP_SPEED[which];
+    brightness_tmp >>= MAX_BRIGHTNESS_SCALE[which];
     brightness[which] = (uint8_t)(brightness_tmp & MAX_BRIGHTNESS_MASK);
   } else {
-    cur_frame -= RAMP_UP_TIME[which];
-    if (cur_frame < RAMP_DOWN_TIME[which]) {
-      brightness_tmp = MAX_BRIGHTNESS[which];
-      brightness_tmp *= (RAMP_DOWN_TIME[which] - cur_frame);
-      brightness_tmp /= RAMP_DOWN_TIME[which];
-      brightness[which] = (uint8_t)(brightness_tmp & MAX_BRIGHTNESS_MASK);
+    if (cur_frame < 0x7FFF) {
+      if (!RAMP_DOWN_SPEED[which]) {
+        cur_frame = 0x8000;
+      } else {
+        brightness_tmp = ~(cur_frame >> 8);
+        cur_frame += RAMP_DOWN_SPEED[which];
+        brightness_tmp >>= MAX_BRIGHTNESS_SCALE[which];
+        brightness[which] = (uint8_t)(brightness_tmp & MAX_BRIGHTNESS_MASK);
+      }
     } else {
       // Waiting INTERVAL
-      brightness[which] = 0;
+      if (!OFF_SPEED[which]) {
+        cur_frame = 0;
+      } else {
+        brightness[which] = 0;
+        cur_frame += ((uint16_t)OFF_SPEED[which]) << 1;
+      }
     }
   }
+  px_frame_no[which] = cur_frame;
+#ifdef DEBUG
   running_brightness = 0;
+#endif
 }
 
+__attribute__((optimize("unroll-loops")))
 void pwm_frame_update(uint8_t frame_no) {
   uint8_t out = 0;
-  for(uint8_t i=0; i<NUM_LEDS; i++) {
+  for(int8_t i=0; i<NUM_LEDS; i++) {
     if(brightness[i] < frame_no)
       out |= (1<<i);
   }
@@ -120,8 +135,11 @@ void pwm_frame_update(uint8_t frame_no) {
 }
 
 // simavr stuff
+AVR_MCU_VCD_FILE("gtkwave_trace.vcd", 300000);
 const struct avr_mmcu_vcd_trace_t _mytrace[]  _MMCU_ = {
   {AVR_MCU_VCD_SYMBOL("update_frame"), .what = &update_frame},
+#ifdef DEBUG
   {AVR_MCU_VCD_SYMBOL("frame_id"), .what = &frame_id},
   {AVR_MCU_VCD_SYMBOL("running_bright"), .what = &running_brightness},
+#endif
 };
